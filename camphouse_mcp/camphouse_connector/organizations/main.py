346f7407f9775e9262396df40b56e7276962215d
RodartHub <rodrigovalencia95@hotmail.com>
2025-08-21 09:45:45 -0500

    edito

--- camphouse_mcp/camphouse_connector/organizations/main.py
+++ camphouse_mcp/camphouse_connector/organizations/main.py
@@ -27,5 +27,20 @@ def get_subsidiaries_organization(organization_id: str) -> Dict[str, List[Dict[s
 
     endpoint = f"organizations/{organization_id}/subsidiaries"
     return make_request(endpoint, method='GET')
+
+
+@mcp.tool(title="Camphouse: Get all partners of an organization")
+def get_list_partners_organization(organization_id: str) -> Dict[str, List[Dict[str, Any]]]:
+    """
+    Camphouse: Get all partners of a specific organization by its ID.
+    Args:
+        organization_id (str): The ID of the organization to retrieve partners for.
+    Returns:
+        Dict[str, List[Dict[str, Any]]]: A dictionary containing a list of dictionaries with the details of each partner.
+    """
+
+    endpoint = f"organizations/{organization_id}/partners"
+    return make_request(endpoint, method='GET')
+    
            
 
